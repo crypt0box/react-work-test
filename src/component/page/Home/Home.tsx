34bd2a2fde7691677c816eaae14946f898169c73
cryptobox <pray,happy76@gmail.com>
2022-05-26 21:21:57 +0900

    feat: override mui

--- src/component/page/Home/Home.tsx
+++ src/component/page/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import styled from 'styled-components';
 import { MuiButton } from '@/component/ui/Button';
 import { MuiTextField } from '@/component/ui/TextField';
 import { Flex } from '@/component/ui/Flex';
@@ -8,6 +9,10 @@ import { useDefaultForm } from '@/component/common/hook';
 type Form = {
   userName: string;
 };
+
+const StyledButton = styled(MuiButton)`
+  background-color: red;
+`;
 
 export const Home = () => {
   const { control, watch } = useDefaultForm<Form>({
@@ -25,9 +30,9 @@ export const Home = () => {
       <h2>Home</h2>
       <Flex flexDirection="column" gap="16px">
         <MuiTextField size="small" helperText="aaa" />
-        <MuiButton variant="outlined" onClick={() => console.log(watch())}>
+        <StyledButton variant="outlined" onClick={() => console.log(watch())}>
           ボタン
-        </MuiButton>
+        </StyledButton>
         <RhfTextArea name="userName" control={control} />
       </Flex>
     </>
